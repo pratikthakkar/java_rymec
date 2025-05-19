69fc04723da3f7e9244a5d0a453e44fe09343cfa
Pramodraj <66528772+pramodppdev@users.noreply.github.com>
2025-05-19 10:59:08 +0530

    Update StudentControllerTest.java

--- src/test/java/com/example/studentmarks/StudentControllerTest.java
+++ src/test/java/com/example/studentmarks/StudentControllerTest.java
@@ -31,7 +31,7 @@ class StudentControllerTest {
         mockMvc.perform(get("/form"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("form"))
-                .andExpect(model().attributeExists("studentabc"));
+                .andExpect(model().attributeExists("student"));
     }
 
     @Test
